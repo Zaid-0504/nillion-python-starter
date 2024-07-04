c12bc3b6fc5e338699496bb81502b5b80cdefa06
Zaid-0504 <itssubi76@gmail.com>
2024-07-04 13:24:02 +0000

    my new nada program

---
+++ quickstart/nada_quickstart_programs/src/main.py
@@ -0,0 +1,22 @@
+from nada_dsl import *
+
+def nada_main():
+    party1 = Party(name="Party1")
+    party2 = Party(name="Party2")
+    party3 = Party(name="Party3")
+    a = SecretInteger(Input(name="A", party=party1))
+    b = SecretInteger(Input(name="B", party=party2))
+
+    b= a%b
+    result : list[SecretInteger] = [] 
+    result.append(a)
+    result.append(b)
+
+    outputs: list[Output] = []
+    for i in range(2):
+        outputs.append(Output(
+            result[i],
+            "result_" + str(i),
+            party3
+        ))
+    return outputs
